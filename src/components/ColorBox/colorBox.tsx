b6b339670c831803d2da21333faeabc9e36c83bc
DominykasSueris <dominykassueris@gmail.com>
2022-08-18 01:53:13 +0100

    element overlay and style added

--- src/components/ColorBox/colorBox.tsx
+++ src/components/ColorBox/colorBox.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Color } from "../../models/SeedColor";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./colorBox.scss";
@@ -7,13 +8,27 @@ interface ColorProps {
 }
 
 const ColorBox = ({ color }: ColorProps) => {
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const handleCopyState = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+    console.log(copied);
+  };
+
   return (
     <div className="ColorBox" style={{ background: color.color }}>
+      <div
+        className={`${copied ? "show" : "copy-overlay"}`}
+        style={{ background: color.color }}
+      ></div>
       <div className="copy-container">
         <div className="box-content">
           <span className="color-name">{color.name}</span>
         </div>
-        <CopyToClipboard text={color.color}>
+        <CopyToClipboard text={color.color} onCopy={handleCopyState}>
           <button className="copy-button">Copy</button>
         </CopyToClipboard>
         <span className="see-more">More</span>
