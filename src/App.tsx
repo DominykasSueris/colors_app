1b9cf658347476a454abf2e1b2dbe2334cc53f5c
DominykasSueris <dominykassueris@gmail.com>
2022-08-19 22:24:38 +0100

    colorHelpers file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Palette from "./components/Pallete/pallete";
 import "./App.css";
 import { seedColors } from "./assets/seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[2]));
   return (
     <div className="App">
       <Palette seedColor={seedColors[1]} />
