ef3778b819c26ebba4d552ee714dc5c36535ceaa
DominykasSueris <dominykassueris@gmail.com>
2022-08-19 23:08:33 +0100

    rc-slider added

--- src/components/Pallete/pallete.tsx
+++ src/components/Pallete/pallete.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { SeedColor } from "../../models/SeedColor";
 import ColorBox from "../ColorBox/colorBox";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import "./pallete.scss";
 
 interface PaletteProps {
@@ -7,8 +10,23 @@ interface PaletteProps {
 }
 
 const Palette = ({ seedColor }: PaletteProps) => {
+  const [sliderLevel, setSliderLevel] = useState<number | number[]>(100);
+
+  const changeLevel = (sliderLevel: number | number[]) => {
+    console.log(sliderLevel);
+    setSliderLevel(sliderLevel);
+  };
+
   return (
     <div className="Palette">
+      <Slider
+        range
+        defaultValue={sliderLevel}
+        min={0}
+        max={900}
+        step={100}
+        onChange={changeLevel}
+      />
       {seedColor.colors.map(color => (
         <ColorBox color={color} />
       ))}
