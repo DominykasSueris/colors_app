a5b3acb19437b84b56c861df3c23074a460f03a7
DominykasSueris <dominykassueris@gmail.com>
2022-08-20 17:40:28 +0100

    added Snackbar with style and positioning

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import Snackbar from "@mui/material/Snackbar";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./navbar.scss";
@@ -13,9 +17,16 @@ interface SliderProps {
 }
 
 const Navbar = ({ sliderLevel, changeLevel, format, changeFormat, setFormat }: SliderProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   const handleChange = (event: SelectChangeEvent) => {
     setFormat(event.target.value);
+    setOpen(true);
     changeFormat(event);
+  };
+
+  const handleSnackbar = () => {
+    setOpen(false);
   };
 
   return (
@@ -43,6 +54,19 @@ const Navbar = ({ sliderLevel, changeLevel, format, changeFormat, setFormat }: S
           <MenuItem value="rgba">RGBA- rgb 255, 255, 255, 1.0</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={3000}
+        message={<span id="message-id">Format changed to {format.toUpperCase()}!</span>}
+        ContentProps={{ "aria-describedby": "message-id" }}
+        onClose={handleSnackbar}
+        action={[
+          <IconButton onClick={handleSnackbar} color="inherit" key="close" aria-label="close">
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
     </header>
   );
 };
