be893b8ff140dc592d2028d1b1cba5e8e051bc46
DominykasSueris <dominykassueris@gmail.com>
2022-08-27 21:13:52 +0100

    ColorHelper component adjusted to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Palette from "./components/Palette/palette";
-import { Route, Routes, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PaletteList from "./components/PaletteList/paletteList";
 import { seedColors } from "./assets/seedColors";
-import { generatePalette } from "./colorHelpers";
-import { SeedColor } from "./models/SeedColor";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Palette seedColor={seedColors[1]} /> */}
       <Routes>
         <Route path="/" element={<PaletteList palettes={seedColors} />}></Route>
         <Route path="/palette/:id" element={<Palette />}></Route>
---
+++ src/colorHelpersTs.ts
@@ -0,0 +1,44 @@
+import { color as chromaColor, scale as chromaScale } from "chroma.ts";
+import { GeneratedColor, GeneratedPalette, SeedColor } from "./models/SeedColor";
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+function generatePalette(starterPalette: SeedColor): GeneratedPalette {
+  let newPalette: GeneratedPalette = {
+    paletteName: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
+    colors: new Map<number, GeneratedColor[]>()
+  };
+
+  for (let level of levels) {
+    newPalette.colors.set(level, []);
+  }
+
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
+    for (let i in scale) {
+      if (newPalette.colors.has(levels[i])) {
+        newPalette.colors.get(levels[i])!.push({
+          name: `${color.name} ${levels[i]}`,
+          id: color.name.toLowerCase().replace(/ /g, "-"),
+          hex: scale[i],
+          rgb: chromaColor(scale[i]).css(),
+          rgba: chromaColor(scale[i]).css().replace("rgb", "rgba").replace(")", ",1.0)")
+        });
+      }
+    }
+  }
+
+  return newPalette;
+}
+
+function getRange(hexColor: string) {
+  const end = "#fff";
+  return [chromaColor(hexColor).darker(1.4).hex(), hexColor, end];
+}
+
+function getScale(hexColor: string, numberOfColors: number) {
+  return chromaScale(getRange(hexColor)).mode("lab").colors(numberOfColors);
+}
+
+export { generatePalette };
