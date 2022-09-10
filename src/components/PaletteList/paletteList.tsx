03c924bf8593161befa0a5c694f00e76eb7bb0f5
DominykasSueris <dominykassueris@gmail.com>
2022-09-10 13:43:03 +0100

    PaletteList basic style added

--- src/components/PaletteList/paletteList.tsx
+++ src/components/PaletteList/paletteList.tsx
@@ -1,5 +1,6 @@
 import { SeedColor } from "../../models/SeedColor";
 import MiniPalette from "../MiniPalette/miniPalette";
+import "./paletteList.scss";
 
 interface SeedColorProps {
   palettes: SeedColor[];
@@ -7,10 +8,17 @@ interface SeedColorProps {
 
 const PaletteList = ({ palettes }: SeedColorProps) => {
   return (
-    <div>
-      {palettes.map(palette => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className="palettelist">
+      <div className="container">
+        <nav className="nav">
+          <h1>React Colors</h1>
+        </nav>
+        <div className="palettes">
+          {palettes.map(palette => (
+            <MiniPalette {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
