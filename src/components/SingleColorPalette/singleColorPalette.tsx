986a9b32b5db6ffc7b5f012b1c8c4658c476cac3
DominykasSueris <dominykassueris@gmail.com>
2022-10-08 23:11:51 +0100

    footer and back button added

--- src/components/SingleColorPalette/singleColorPalette.tsx
+++ src/components/SingleColorPalette/singleColorPalette.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router";
-import { generateColorScale } from "../../assets/colorHelpersTs";
+import { Link } from "react-router-dom";
+import { generateColorScale, generatePalette } from "../../assets/colorHelpersTs";
 import { findPalleteById } from "../../assets/seedColors";
 import ColorBox from "../ColorBox/colorBox";
 import { ColorFormatType } from "../../models/SeedColor";
@@ -11,9 +12,9 @@ import PaletteFooter from "../PaletteFooter/paletteFooter";
 const SingleColorPalette = () => {
   const [format, setFormat] = useState<ColorFormatType>("hex");
   const { paletteId, colorName } = useParams();
+  const seedColor = findPalleteById(paletteId);
 
   const smallColor = () => {
-    const seedColor = findPalleteById(paletteId);
     if (!seedColor) {
       throw new Error("Seed Color not Found");
     }
@@ -25,11 +26,17 @@ const SingleColorPalette = () => {
     return (
       <>
         {generateColorScale(color).map(color => {
-          console.log(color);
           return <ColorBox color={color} format={format} key={color.name} />;
         })}
       </>
     );
+  };
+
+  const footer = () => {
+    if (!seedColor) {
+      throw new Error("Seed Color not Found");
+    }
+    return <PaletteFooter generatedPalette={generatePalette(seedColor)} />;
   };
 
   return (
@@ -38,9 +45,13 @@ const SingleColorPalette = () => {
       <h1>Single Color Palette</h1>
       <div className="Palette-colors">
         {smallColor()}
-        <div className="go-back">Go back</div>
+        <div className="go-back ColorBox">
+          <Link to={`/palette/${paletteId}`} className="back-button">
+            GO BACK
+          </Link>
+        </div>
       </div>
-      <PaletteFooter generatedPalette={color} />
+      {footer()}
     </div>
   );
 };
