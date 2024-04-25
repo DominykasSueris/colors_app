4f94e2ff905338b2fd4f148dc4393c0a12c919cd
Dominykas Sueris <dominykassueris@gmail.com>
2024-04-25 13:40:26 +0100

    deploy file adjustment

--- src/bin/deploy.sh
+++ src/bin/deploy.sh
@@ -2,6 +2,6 @@
 
 git pull && docker build -t colors_app .
 
-docker kill portfolio && docker rm colors_app
+docker kill colors_app && docker rm colors_app
 
 docker run -p 9081:3000 -d --name colors_app colors_app
