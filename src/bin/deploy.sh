b57fa463bb9d157e4561cee12552fef3a70a6281
Dominykas Sueris <dominykassueris@gmail.com>
2024-04-25 13:32:36 +0100

    port changes

--- src/bin/deploy.sh
+++ src/bin/deploy.sh
@@ -4,4 +4,4 @@ git pull && docker build -t colors_app .
 
 docker kill portfolio && docker rm colors_app
 
-docker run -p 9080:3000 -d --name colors_app colors_app
+docker run -p 9081:3000 -d --name colors_app colors_app
